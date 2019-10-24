d510fccb306ebb193cab8a01b267bf8dc283ac17
Piotr Kowalski <piecioshka@gmail.com>
2019-10-24 12:39:02 +0200

    Add new rules

--- index.js
+++ index.js
@@ -2,11 +2,27 @@ module.exports = {
     "extends": [
         "tslint:recommended"
     ],
+
+    // https://palantir.github.io/tslint/rules/
     rules: {
         "variable-name": {
             "options": [
                 "allow-leading-underscore"
             ]
         },
+        "member-ordering": [true, { "order": "statics-first" }],
+        "member-access": [true, "no-public"],
+        "array-type": [true, "generic"],
+        "space-before-function-paren": [true, {
+            "anonymous": "always",
+            "asyncArrow": "always",
+            "constructor": "never",
+            "method": "never",
+            "named": "never",
+        }],
+        "no-var-requires": false,
+        "no-console": false,
+        "trailing-comma": false,
+        "quotemark": [true, "single"]
     }
 };
